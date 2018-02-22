568fa4e8d1c9a789561464cccab6217f86595e8c
Sebastian <sebasdeldi123@gmail.com>
2018-02-22 17:38:37 -0500

    identifying roles on views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,29 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def is_admin?
+  	current_role == 'admin' ? true : false
+  end
+  helper_method :is_admin?
+
+  def is_agent?
+  	current_role == 'agent' ? true : false
+  end
+  helper_method :is_agent?
+
+  def is_consignee?
+  	current_role == 'consignee' ? true : false
+  end
+  helper_method :is_consignee?
+
+  def is_shipper?
+  	current_role == 'shipper' ? true : false
+  end
+  helper_method :is_shipper?
+
+  private
+  	def current_role
+  		current_role = Role.find(current_user.role_id).name
+  	end
+
 end
