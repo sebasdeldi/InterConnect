62f70d7d7735fe01acee62ab8e0cb01ce758efb8
Sebastian <sebasdeldi123@gmail.com>
2018-02-23 16:07:55 -0500

    Creating others accounts only by admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,29 +1,48 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  helper_method :resource_name, :resource, :devise_mapping, :resource_class, :is_admin?, :is_agent?, :is_consignee?,
+    :is_shipper?
+
+
+  # Roles detection helpers
   def is_admin?
   	current_role == 'admin' ? true : false
   end
-  helper_method :is_admin?
 
   def is_agent?
   	current_role == 'agent' ? true : false
   end
-  helper_method :is_agent?
 
   def is_consignee?
   	current_role == 'consignee' ? true : false
   end
-  helper_method :is_consignee?
 
   def is_shipper?
   	current_role == 'shipper' ? true : false
   end
-  helper_method :is_shipper?
+
+
+  # Devise helpers
+  def resource_name
+    :user
+  end
+ 
+  def resource
+    @resource ||= User.new
+  end
+
+  def resource_class
+    User
+  end
+ 
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
 
   private
   	def current_role
-  		current_role = Role.find(current_user.role_id).name
+  		current_role = current_user.nil? ? 'Not logged in' : Role.find(current_user.role_id).name 
   	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+ 	devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
 
   devise_scope :user do
 	  authenticated :user do
