be84d234d9f7bda6463b12107b8a21839abb8979
Sebastian <sebasdeldi123@gmail.com>
2018-10-16 10:29:36 -0400

    a

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,7 +87,7 @@ Rails.application.configure do
   end
 
   # Mailer settings
-  #config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address        => 'smtp.office365.com',
     :port           => '587',
