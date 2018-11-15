9cf13e10829aaf0e0c9dd19b952198823fcab3e2
Sebastian <sebasdeldi123@gmail.com>
2018-11-15 17:26:27 -0500

    mod seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,20 +20,20 @@ roles = Role.create!([
 ])
 
 users = User.create!([
-	{email: 'admin@admin.com', password: '12345678', role_id: 1, company_name: 'Interworld Freight', phone_number: '7862803736', contact_first_name: 'Pepito', contact_last_name: 'Perez'},
-	{email: 'leader@leader.com', password: '12345678', role_id: 10, company_name: 'Interworld Freight', phone_number: '7862803736', contact_first_name: 'Daniel', contact_last_name: 'Zapata'},
-	{email: 'sdelgado@interwf.com', password: '12345678', role_id: 2, company_name: 'Interworld Freight', phone_number: '7862803736', outlook_password: 'Sdiwf**321', contact_first_name: 'Sebastián', contact_last_name: 'Delgado',
+	{email: 'admin@admin.com', password: '12345678', role_id: 1, company_name: 'Interworld Freight', phone_number: '23423423', contact_first_name: 'Pepito', contact_last_name: 'Perez'},
+	{email: 'leader@leader.com', password: '12345678', role_id: 10, company_name: 'Interworld Freight', phone_number: '12313123', contact_first_name: 'Daniel', contact_last_name: 'Zapata'},
+	{email: 'sdelgado@interwf.com', password: '12345678', role_id: 2, company_name: 'Interworld Freight', phone_number: '53435453', outlook_password: 'Sdiwf**321', contact_first_name: 'Sebastián', contact_last_name: 'Delgado',
 	address: '7741 nw 7th st', state: 'Florida', country: 'USA', city: 'Miami', zip_code: 33126 },
-	{email: 'maria@interwf.com', password: '12345678', role_id: 2, company_name: 'Interworld Freight', phone_number: '7862803736', outlook_password: 'Sdiwf**321', contact_first_name: 'Maria', contact_last_name: 'Dolores' },
-	{email: 'juan@interwf.com', password: '12345678', role_id: 2, company_name: 'Interworld Freight', phone_number: '7862803736', outlook_password: 'Sdiwf**321', contact_first_name: 'Juanito', contact_last_name: 'Pérez' },
-	{email: 'agent@agent.com', password: '12345678', role_id: 3 , company_name: 'Interworld Bogotá', phone_number: '7862803736', contact_first_name: 'J', contact_last_name: 'Balvin'},
-	{email: 'agent1@agent.com', password: '12345678', role_id: 3 , company_name: 'Interworld Bogotá', phone_number: '7862803736', contact_first_name: 'Juan Luis', contact_last_name: 'Londoño'},
-	{email: 'agent2@agent.com', password: '12345678', role_id: 3 , company_name: 'Interworld Bogotá', phone_number: '7862803736', contact_first_name: 'Carlos', contact_last_name: 'Vives'},
-	{email: 'agent3@agent.com', password: '12345678', role_id: 3 , company_name: 'Interworld Bogotá', phone_number: '7862803736', contact_first_name: 'Juanes', contact_last_name: 'Aristizabal'},
+	{email: 'maria@interwf.com', password: '12345678', role_id: 2, company_name: 'Interworld Freight', phone_number: '234234234', outlook_password: 'Sdiwf**321', contact_first_name: 'Maria', contact_last_name: 'Dolores' },
+	{email: 'juan@interwf.com', password: '12345678', role_id: 2, company_name: 'Interworld Freight', phone_number: '08954563', outlook_password: 'Sdiwf**321', contact_first_name: 'Juanito', contact_last_name: 'Pérez' },
+	{email: 'vrgargo@agent.com', password: '12345678', role_id: 3 , company_name: 'Vr Cargo', phone_number: '464564645', contact_first_name: 'J', contact_last_name: 'Balvin'},
+	{email: 'gamacargo@agent.com', password: '12345678', role_id: 3 , company_name: 'Gama Cargo', phone_number: '53543543', contact_first_name: 'Juan Luis', contact_last_name: 'Londoño'},
+	{email: 'corlogistic@agent.com', password: '12345678', role_id: 3 , company_name: 'Corlogistic', phone_number: '354354345', contact_first_name: 'Carlos', contact_last_name: 'Vives'},
+	{email: 'masterlog@agent.com', password: '12345678', role_id: 3 , company_name: 'Masterlogistic', phone_number: '36544565', contact_first_name: 'Juanes', contact_last_name: 'Aristizabal'},
 
-	{email: 'shipper@shipper.com', password: '12345678', role_id: 4, company_name: 'Super Sipper', phone_number: '7862803736', contact_first_name: 'Camila', contact_last_name: 'Díaz'},
-	{email: 'shippe1@shipper.com', password: '12345678', role_id: 4, company_name: 'Super Sipper', phone_number: '7862803736', contact_first_name: 'Pepita', contact_last_name: 'Chacón'},
-	{email: 'shipper2@shipper.com', password: '12345678', role_id: 4, company_name: 'Super Sipper', phone_number: '7862803736', contact_first_name: 'Fulanita', contact_last_name: 'Pulido'},
+	{email: 'corona@shipper.com', password: '12345678', role_id: 4, company_name: 'Corona', phone_number: '7862803736', contact_first_name: 'Camila', contact_last_name: 'Díaz'},
+	{email: 'pilsen@shipper.com', password: '12345678', role_id: 4, company_name: 'Pilsen', phone_number: '7862803736', contact_first_name: 'Pepita', contact_last_name: 'Chacón'},
+	{email: 'aguila@shipper.com', password: '12345678', role_id: 4, company_name: 'Aguila', phone_number: '7862803736', contact_first_name: 'Fulanita', contact_last_name: 'Pulido'},
 
 	{email: 'pricing@pricing.com', password: '12345678', role_id: 7, company_name: 'IWF', phone_number: '7862803736'},
 	{email: 'consignee@consignee.com', password: '12345678', role_id: 8, company_name: 'Consigneelandia', phone_number: '7862803736', outlook_password: 'Sdiwf**321', contact_first_name: 'Sebastián', contact_last_name: 'Delgado',
