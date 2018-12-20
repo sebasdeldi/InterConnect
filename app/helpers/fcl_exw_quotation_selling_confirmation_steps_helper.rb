ff8eb1095caf34f5a92298f2817660abbacdaf54
Sebastian <sebasdeldi123@gmail.com>
2018-12-20 09:19:21 -0500

    fixing docs

--- app/helpers/fcl_exw_quotation_selling_confirmation_steps_helper.rb
+++ app/helpers/fcl_exw_quotation_selling_confirmation_steps_helper.rb
@@ -2,4 +2,14 @@ module FclExwQuotationSellingConfirmationStepsHelper
 	def quotation_confirmation_price(operation_id)
 		FclExwQuotationSellingConfirmationStep.find_by(operation_id: operation_id)
 	end
+
+	def quotation_total(quotation)
+		profit = quotation.profit.nil? ? 0 : quotation.profit
+		documentation = quotation.documentation.nil? ? 0 : quotation.documentation
+		ff = quotation.ff.nil? ? 0 : quotation.ff
+		vgm = quotation.vgm.nil? ? 0 : quotation.vgm 
+		inland = quotation.inland.nil? ? 0 : quotation.inland 
+		others = quotation.others.nil? ? 0 : quotation.others
+		profit + documentation + ff + vgm + inland + others
+	end
 end
